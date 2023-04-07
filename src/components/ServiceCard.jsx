906ba1cf81eb982d22b9d0f2b4474030f71c8420
Daniyar <ensw01119@student.su.edu.krd>
2023-04-07 20:39:03 +0300

    .

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -17,7 +17,13 @@ export default function ServiceCard({
         aria-controls={additions.ariaControls}
         aria-selected={additions.ariaSelected}
       >
-        {svgIcon}
+        {
+          <img
+            src="src/assets/images/services/consultation.png"
+            height="60px"
+            width="60px"
+          />
+        }
         <span>
           {cardTitle.substr(0, cardTitle.indexOf(" "))} <br />
           {cardTitle.substr(cardTitle.indexOf(" "), cardTitle.length)}
