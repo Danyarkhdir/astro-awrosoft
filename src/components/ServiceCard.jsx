cf1d4763d4ad29729825ec5f5cfe5860ff90dab3
Daniyar <ensw01119@student.su.edu.krd>
2023-04-08 01:35:05 +0300

    back to default without path solution

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -17,13 +17,7 @@ export default function ServiceCard({
         aria-controls={additions.ariaControls}
         aria-selected={additions.ariaSelected}
       >
-        {
-          <img
-            src="src/assets/images/services/consultation.png"
-            height="60px"
-            width="60px"
-          />
-        }
+        {svgIcon}
         <span>
           {cardTitle.substr(0, cardTitle.indexOf(" "))} <br />
           {cardTitle.substr(cardTitle.indexOf(" "), cardTitle.length)}
